ec5f542ed933c1cbdfd66ede8c83526df00084eb
Connor Bowlan <cbowlan@tbb.bank>
2022-08-09 14:59:51 -0500

    Added a sample application for people to use for testing implementation.

---
+++ SampleApplication.Web/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using SampleApplication.Web.Models;
+
+namespace SampleApplication.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(SampleModel model)
+        {
+            return View(model);
+        }
+    }
+}
---
+++ SampleApplication.Web/Models/SampleModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SampleApplication.Web.Models
+{
+    public class SampleModel
+    {
+        [Required]
+        public string Name { get; set; }
+    }
+}
