57f9895a39dbba228aa02d2817d0c494f0a97c25
Connor Bowlan <cbowlan@tbb.bank>
2022-08-09 16:01:49 -0500

    Adding more properties to model and view for sample project.

--- SampleApplication.Web/Models/SampleModel.cs
+++ SampleApplication.Web/Models/SampleModel.cs
@@ -1,10 +1,26 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace SampleApplication.Web.Models
 {
     public class SampleModel
     {
-        [Required]
+        [Required, Display(Name = "What is your name?")]
         public string Name { get; set; }
+
+        [Required, Display(Name = "What is your email address?")]
+        public string Email { get; set; }
+
+        [Required, Display(Name = "What is your phone number?"), MinLength(10), MaxLength(10)]
+        public string PhoneNumber { get; set; }
+
+        [Required, Display(Name = "What is your birthday?")]
+        public DateTime Birthday { get; set; }
+
+        [Required, DataType(DataType.Currency), Display(Name = "What salary would you like to make this year?")]
+        public double Salary { get; set; }
+
+        [Required, Display(Name = "What is your favorite color?")]
+        public string Color { get; set; }
     }
 }
