02b0af2db105eea0ff248ecb8773f41fc263cf26
Connor Bowlan <cbowlan@tbb.bank>
2022-08-12 13:48:33 -0500

    Building examples and documentation for existing functionality.

--- SampleApplication.Web/Models/SampleModel.cs
+++ SampleApplication.Web/Models/SampleModel.cs
@@ -5,22 +5,40 @@ namespace SampleApplication.Web.Models
 {
     public class SampleModel
     {
-        [Required, Display(Name = "What is your name?")]
+        [Required, Display(Name = "Name")]
         public string Name { get; set; }
 
-        [Required, Display(Name = "What is your email address?")]
+        [Required, Display(Name = "Email Address")]
         public string Email { get; set; }
 
-        [Required, Display(Name = "What is your phone number?"), MinLength(10), MaxLength(10)]
+        [Required, Display(Name = "Phone Number")]
         public string PhoneNumber { get; set; }
 
-        [Required, Display(Name = "What is your birthday?")]
-        public DateTime Birthday { get; set; }
+        [Required, Display(Name = "Date of Birth (string)")]
+        public string DateOfBirthString { get; set; }
 
-        [Required, DataType(DataType.Currency), Display(Name = "What salary would you like to make this year?")]
+        [Required, Display(Name = "File")]
+        public string File { get; set; }
+
+        // Some data types are typically not nullable. However, we can add "?" after the
+        // property type to establish it is nullable.
+        // This will reflect in the front-end validation as well.
+        [Display(Name = "Date of Birth (DateTime)")]
+        public DateTime DateOfBirth { get; set; }
+
+        [Display(Name = "Color")]
+        public string Color { get; set; }
+
+        [Display(Name = "Option")]
+        public bool Option { get; set; }
+
+        // Some data types are typically not nullable. However, we can add "?" after the
+        // property type to establish it is nullable.
+        // This will reflect in the front-end validation as well.
+        [DataType(DataType.Currency), Display(Name = "What salary would you like to make this year?")]
         public double Salary { get; set; }
 
-        [Required, Display(Name = "What is your favorite color?")]
-        public string Color { get; set; }
+        [Display(Name = "Enable super cool feature?")]
+        public bool SuperCoolFeature { get; set; }
     }
 }
